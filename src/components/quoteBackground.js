6b4f90f2d2909f7e0b94a1888e80024037b02c18
stephenwelch1927 <stephenwelch1927@yahoo.com>
2023-04-24 16:16:57 +0700

    Added bootstrap quote marks

--- src/components/quoteBackground.js
+++ src/components/quoteBackground.js
@@ -1,14 +1,15 @@
 import back from './quote.module.css';
 import React from "react";
 import Card from "react-bootstrap/Card";
-
+import {BsFillChatLeftQuoteFill} from 'react-icons/bs';
+import {BsFillChatRightQuoteFill} from 'react-icons/bs';
 class QuoteBackground extends React.Component {
   render() {
     return (
       <div className={back.box}>
         <div className={back.card}>
             <h1 className={back.heading}>Random Quote Generator</h1>
-            <h2>Quote</h2>
+            <h2>Quote <BsFillChatLeftQuoteFill /><BsFillChatRightQuoteFill/></h2>
         </div>
       </div>
     );
