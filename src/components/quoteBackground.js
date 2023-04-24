10eb6f36e6c467ab72dd28826d3e0250fb96f0b8
stephenwelch1927 <stephenwelch1927@yahoo.com>
2023-04-24 17:25:15 +0700

    Added button feature

--- src/components/quoteBackground.js
+++ src/components/quoteBackground.js
@@ -1,22 +1,31 @@
-import back from './quote.module.css';
+import back from "./quote.module.css";
 import React from "react";
 import Card from "react-bootstrap/Card";
-import {BsFillChatLeftQuoteFill} from 'react-icons/bs';
-import {BsFillChatRightQuoteFill} from 'react-icons/bs';
-import {BsHandIndexThumbFill} from 'react-icons/bs';
+import { BsFillChatLeftQuoteFill } from "react-icons/bs";
+import { BsFillChatRightQuoteFill } from "react-icons/bs";
+import { BsHandIndexThumbFill } from "react-icons/bs";
+import Button from 'react-bootstrap/Button';
 
 class QuoteBackground extends React.Component {
   render() {
     return (
       <div className={back.box}>
         <div className={back.card}>
-            <h1 className={back.heading}>Random Quote Generator</h1>
-            <div id="text">
-            <h2>Quote <BsFillChatLeftQuoteFill /><BsFillChatRightQuoteFill/></h2>
-            </div>
-            <div id="author">
-                <h2>Author <BsHandIndexThumbFill/></h2>
-            </div>
+          <h1 className={back.heading}>Random Quote Generator</h1>
+          <div id="text">
+            <h2>
+              Quote <BsFillChatLeftQuoteFill />
+              <BsFillChatRightQuoteFill />
+            </h2>
+          </div>
+          <div id="author">
+            <h2>
+              Author <BsHandIndexThumbFill />
+            </h2>
+          </div>
+            <div id="new-quote" className={back.button}>
+          <Button variant="red" size="lg">Generate New Quote</Button>
+          </div>
         </div>
       </div>
     );
