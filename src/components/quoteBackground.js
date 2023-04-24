65718d600725f869f7d90c151cdeb2152633f1f1
stephenwelch1927 <stephenwelch1927@yahoo.com>
2023-04-24 16:43:31 +0700

    Add aditional author icon

--- src/components/quoteBackground.js
+++ src/components/quoteBackground.js
@@ -3,13 +3,20 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import {BsFillChatLeftQuoteFill} from 'react-icons/bs';
 import {BsFillChatRightQuoteFill} from 'react-icons/bs';
+import {BsHandIndexThumbFill} from 'react-icons/bs';
+
 class QuoteBackground extends React.Component {
   render() {
     return (
       <div className={back.box}>
         <div className={back.card}>
             <h1 className={back.heading}>Random Quote Generator</h1>
+            <div id="text">
             <h2>Quote <BsFillChatLeftQuoteFill /><BsFillChatRightQuoteFill/></h2>
+            </div>
+            <div id="author">
+                <h2>Author <BsHandIndexThumbFill/></h2>
+            </div>
         </div>
       </div>
     );
