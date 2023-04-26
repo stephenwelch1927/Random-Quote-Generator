b944bf433fa1d6122f0c76ef121d57441f464eb8
stephenwelch1927 <stephenwelch1927@yahoo.com>
2023-04-26 17:04:27 +0700

    Tidy up UI and add css

--- src/components/quote1.js
+++ src/components/quote1.js
@@ -2,53 +2,57 @@ import React from "react";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
-import back from './quote.module.css';
+import back from "./quote.module.css";
 import { BsFillChatLeftQuoteFill } from "react-icons/bs";
 import { BsFillChatRightQuoteFill } from "react-icons/bs";
+import { BsHandIndexThumbFill } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
 
 function ApiCall() {
-   
-    const [quote, setQuote] = useState(' ');
-    const [author, setAuthor] = useState('');
-    
-    const retrieveQuote = () => {
-        axios
-          .get("https://official-joke-api.appspot.com/random_joke")
-          .then((response) => {
-            console.log(response);
-            setQuote(response.data.setup)
-            setAuthor(response.data.punchline)
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      };
+  const [quote, setQuote] = useState(" ");
+  const [author, setAuthor] = useState("");
 
-      const display = (props) => {
-        return(
-            <div>
-                {quote};
-            </div>
-        );
-      }
+  const retrieveQuote = () => {
+    axios
+      .get("https://official-joke-api.appspot.com/random_joke")
+      .then((response) => {
+        console.log(response);
+        setQuote(response.data.setup);
+        setAuthor(response.data.punchline);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-      return(
-        <div>
+  return (
+    <div>
+      <div id="new-quote" className={back.button}>
         <Button variant="flat" size="xxl" onClick={retrieveQuote}>
-        Generate Random Quote
-      </Button>
+          Generate Random Quote
+        </Button>
+      </div>
       <div id="text" className={back.displayQuote}>
-      <p><h2>Quote</h2><BsFillChatLeftQuoteFill />{quote}<BsFillChatRightQuoteFill /></p> 
-        </div>
-        
-        <p className={back.displayAuthor}></p>
-        </div>
-        
-      );
-    
+        <h2>Quote</h2>
+        <p className={back.textDisplay}>
+          <BsFillChatLeftQuoteFill />
+          {quote}
+          <BsFillChatRightQuoteFill />
+        </p>
+      </div>
 
+      <div id="author" className={back.displayAuthor}>
+        <h2>
+          Author <BsHandIndexThumbFill />
+        </h2>
+        <p>{author}</p>
+      </div>
+      <div id="tweet-one">
+        <a href="https://twitter.com/" target="_blank">
+          <BsTwitter className={back.icon} />
+        </a>
+      </div>
+    </div>
+  );
 }
 export default ApiCall;
-
-
-
--- src/components/quoteBackground.js
+++ src/components/quoteBackground.js
@@ -1,48 +1,23 @@
 import back from "./quote.module.css";
 import React from "react";
-import Card from "react-bootstrap/Card";
-import { BsFillChatLeftQuoteFill } from "react-icons/bs";
-import { BsFillChatRightQuoteFill } from "react-icons/bs";
-import { BsHandIndexThumbFill } from "react-icons/bs";
-import { BsTwitter } from "react-icons/bs";
-import axios from "axios";
-import './getQuote';
+import "./getQuote";
 import GetQuote from "./getQuote";
-import './quote1';
+import "./quote1";
 import ApiCall from "./quote1";
-import quotePass from './quote1';
-import display from './quote1';
+import quotePass from "./quote1";
+import display from "./quote1";
 
 class QuoteBackground extends React.Component {
   constructor(props) {
     super(props);
   }
 
-
   render() {
     return (
       <div className={back.box}>
-        <div className={back.card}>
-          <h1 className={back.heading}>Random Quote Generator</h1>
-          <div id="text">
-            <h2>
- 
-            </h2>
-          </div>
-          <div id="author" className={back.displayAuthor}>
-            <h2>
-              Author <BsHandIndexThumbFill />
-            </h2>
-          </div>
-          <div id="new-quote" className={back.button}>
-            <ApiCall />
-          </div>
-          <div id="tweet-one">
-            <a href="https://twitter.com/" target="_blank">
-              <BsTwitter className={back.icon} />
-            </a>
-          </div>
-        </div>
+        <h1 className={back.heading}>Random Quote Generator</h1>
+
+        <ApiCall />
       </div>
     );
   }
